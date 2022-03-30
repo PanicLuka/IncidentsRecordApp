bba3f3064eecf047dcc204b63fc72c3da1f977ba
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:51:43 +0200

    dodat prazan kontroler za Category

---
+++ backend/IncidentService/Controllers/CategoryController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IncidentService.Controllers
+{
+    [ApiController]
+    [Route("api/category")]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
