54d7ec52675bdf09c752f88a8038cc04f2e5a758
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 12:04:26 +0200

    Dodat profil za Category

---
+++ backend/IncidentService/Profiles/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using IncidentService.Entities;
+using IncidentService.Models;
+
+namespace IncidentService.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+        }
+    }
+}
