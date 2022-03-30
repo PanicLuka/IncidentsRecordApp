40af124273562694a44206607c0f303b4946c419
PanicLuka <lukap181@gmail.com>
2022-03-30 13:28:29 +0200

    Role entity created

---
+++ backend/UserService/Enitites/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Enitites
+{
+    
+    public class Role
+    {
+        #region
+        public int RoleId { get; set; }
+
+        public string UserType { get; set; }
+
+        #endregion
+    }
+}
