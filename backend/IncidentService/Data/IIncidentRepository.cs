27e86a200d600dc5ca0dd1856741a9f3f4c458ec
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:15:09 +0200

    Dodat interfejs IIncidentRepository

---
+++ backend/IncidentService/Data/IIncidentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using IncidentService.Entities;
+
+namespace IncidentService.Data
+{
+    public interface IIncidentRepository
+    {
+        List<Incident> GetIncidents();
+        Incident GetIncidentById(int id);
+        void UpdateIncident(Incident incident);
+        void DeleteIncident(int id);
+        void CreateIncident(Incident incident);
+        
+    }
+}
