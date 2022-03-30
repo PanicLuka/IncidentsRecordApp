7733ed441e92342dc0371caa43b5fb9e0e0fa2a9
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:26:40 +0200

    Dodat CategoryRepository

---
+++ backend/IncidentService/Data/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using IncidentService.Entities;
+
+namespace IncidentService.Data
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public void CreateIncident(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteIncident(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Incident GetIncidentById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Category> GetIncidents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateIncident(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
