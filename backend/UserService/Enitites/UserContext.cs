782718bcf660386dceecb13e3da86a3e1b45758c
PanicLuka <lukap181@gmail.com>
2022-03-30 15:01:10 +0200

    Context class implemented

--- backend/UserService/Enitites/UserContext.cs
+++ backend/UserService/Enitites/UserContext.cs
@@ -1,11 +1,55 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace UserService.Enitites
 {
-    public class UserContext
+    public class UserContext : DbContext
     {
+        private readonly IConfiguration configuration;
+
+        public UserContext(DbContextOptions<UserContext> options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<Register> register { get; set; }
+
+        public DbSet<Role> roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("UserRegisterDB"));
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Role>()
+                .HasData(new
+                {
+                    UserType = "User"
+                });
+
+
+
+            modelBuilder.Entity<Register>()
+                .HasData(new
+                {
+                    UserId = 1,
+                    FirstName = "Marko",
+                    LastName = "Milic",
+                    Email = "marko@gmail.com",
+                    Password = "123456",
+                    RoleId = 2
+
+
+                });
+        }
+
     }
 }
