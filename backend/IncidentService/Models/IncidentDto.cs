9ca59ec29a5c14902af7ec9f8c62c8891ae92a39
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:05:02 +0200

    Dodata dto klasa za incident

---
+++ backend/IncidentService/Models/IncidentDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IncidentService.Models
+{
+    public class IncidentDto
+    {
+        public string Number { get; set; }
+        public int Significance { get; set; }
+        public string Workspace { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public bool ThirdPartyHelp { get; set; }
+        public string ProblemSolved { get; set; }
+        public bool FurtherAction { get; set; }
+        public string FurtherActionPerson { get; set; }
+        public string ActionDescription { get; set; }
+        public DateTime SolvingDate { get; set; }
+        public string Remarks { get; set; }
+        public string Verifies { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
