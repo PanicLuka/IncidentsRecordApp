8e22f08d0194beacc5ddd4bb3b4b37e060dffc0d
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:17:25 +0200

    Dodat IncidentRepository

---
+++ backend/IncidentService/Data/IncidentRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using IncidentService.Entities;
+
+namespace IncidentService.Data
+{
+    public class IncidentRepository : IIncidentRepository
+    {
+        public void CreateIncident(Incident incident)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteIncident(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Incident GetIncidentById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Incident> GetIncidents()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateIncident(Incident incident)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
