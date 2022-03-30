ec06754e9619b0248f473c8ea1f570262bdeb41b
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:50:04 +0200

    Dodat prazan kontroler za Incident

---
+++ backend/IncidentService/Controllers/IncidentController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IncidentService.Controllers
+{
+    [ApiController]
+    [Route("api/incident")]
+    public class IncidentController : ControllerBase
+    {
+    }
+}
