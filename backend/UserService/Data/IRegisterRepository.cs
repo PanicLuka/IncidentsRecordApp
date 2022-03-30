66ee64dcd9cfbb0b1ae50bd1472a760a54c19734
PanicLuka <lukap181@gmail.com>
2022-03-30 14:36:52 +0200

    Interfaces added for classes register and role

---
+++ backend/UserService/Data/IRegisterRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+
+namespace UserService.Data
+{
+    public interface IRegisterRepository
+    {
+        Task CreateUserAsync(Register User);
+
+        Task<List<Register>> GetAllUsersAsync();
+
+        Task<Register> GetUserByIdAsync(int UserId);
+
+        Task UpdateUserAsync(Register User);
+
+        Task DeleteUserAsync(int UserId);
+
+        Task<bool> SaveChangesAsync();
+
+    }
+}
---
+++ backend/UserService/Data/IRoleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+
+namespace UserService.Data
+{
+    public interface IRoleRepository
+    {
+        Task CreateRoleAysnc (Role Role);
+
+        Task<List<Role>> GetAllRolesAsync();
+
+        Task<Role> GetRoleByIdAsync(int RoleId);
+
+        Task UpdateRoleAsync(Role Role);
+
+        Task DeleteRoleAsync(int RoleId);
+
+        Task<bool> SaveChangesAsync();
+
+    }
+}
