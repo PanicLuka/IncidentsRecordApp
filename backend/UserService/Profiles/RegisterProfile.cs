6fe60ecdcd90ac4df9eb7481771be3f1c232d883
PanicLuka <lukap181@gmail.com>
2022-03-30 15:37:54 +0200

    Profile classes created

---
+++ backend/UserService/Profiles/RegisterProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+using UserService.Models;
+
+namespace UserService.Profiles
+{
+    public class RegisterProfile : Profile
+    {
+        public RegisterProfile()
+        {
+            CreateMap<Register, RegisterDto>();
+            CreateMap<RegisterDto, Register>();
+        }
+    }
+}
---
+++ backend/UserService/Profiles/RoleProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+using UserService.Models;
+
+namespace UserService.Profiles
+{
+    public class RoleProfile : Profile
+    {
+        public RoleProfile()
+        {
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleDto, Role>();
+
+        }
+    }
+}
