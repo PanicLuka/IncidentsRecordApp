dbfce4ada7cb61821f1a709ff5a45b48dfd28d5d
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-03-30 11:24:42 +0200

    Dodat interfejs ICategoryRepository

---
+++ backend/IncidentService/Data/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using IncidentService.Entities;
+
+namespace IncidentService.Data
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetIncidents();
+        Incident GetIncidentById(int id);
+        void UpdateIncident(Category category);
+        void DeleteIncident(int id);
+        void CreateIncident(Category category);
+    }
+}
