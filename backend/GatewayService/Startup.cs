1ee007656481d4d1759302def26d794187710391
Kristijan Stošić <nimeria83@yahoo.com>
2022-03-31 12:01:13 +0200

    13-mvp-gateway-service-setting up gateway routes for other services

--- backend/GatewayService/Startup.cs
+++ backend/GatewayService/Startup.cs
@@ -1,18 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Ocelot.Cache.CacheManager;
 
 namespace GatewayService
@@ -31,6 +24,9 @@ namespace GatewayService
         {
 
             services.AddControllers();
+            services.AddSwaggerForOcelot(Configuration);
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GatewayService", Version = "v1" });
@@ -44,9 +40,11 @@ namespace GatewayService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GatewayService v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GatewayService v1"));
             }
+
+            app.UseSwaggerForOcelotUI();
 
             app.UseHttpsRedirection();
 
