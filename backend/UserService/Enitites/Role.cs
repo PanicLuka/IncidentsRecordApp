2e9afceec127416c541b358012f8210cce2c36d6
PanicLuka <lukap181@gmail.com>
2022-04-01 09:05:36 +0200

    Role class modified

--- backend/UserService/Enitites/Role.cs
+++ backend/UserService/Enitites/Role.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,8 @@ namespace UserService.Enitites
     public class Role
     {
         #region
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RoleId { get; set; }
 
         public string UserType { get; set; }
