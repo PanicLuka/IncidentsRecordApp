ae3c682acb864f0939efd8a8429a2e688e793ae0
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-01 09:42:45 +0200

    Fluent validator for vategory added

---
+++ backend/IncidentService/Validators/CategoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using IncidentService.Entities;
+
+namespace IncidentService.Validators
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Category name must be entered!");
+        }
+    }
+}
