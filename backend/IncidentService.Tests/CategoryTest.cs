00f3c6364b8721c99ec1c985c5aeeafb3f474e5f
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-01 16:17:02 +0200

    Setup for testing

---
+++ backend/IncidentService.Tests/CategoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IncidentService.Controllers;
+using IncidentService.Data;
+using Moq;
+using Xunit;
+
+namespace IncidentService.Tests
+{
+    public class CategoryTest
+    {
+        public Mock<ICategoryRepository> mock = new Mock<ICategoryRepository>();
+
+        [Fact]
+        public async void GetCategoryByIdAsync()
+        {
+            
+        }
+    }
+}
---
+++ backend/IncidentService.Tests/IncidentTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IncidentService.Data;
+using Moq;
+using Xunit;
+
+namespace IncidentService.Tests
+{
+    public class IncidentTest
+    {
+        public Mock<IIncidentRepository> mock = new Mock<IIncidentRepository>();
+
+        [Fact]
+        public async void GetIncidentByIdAsync()
+        {
+
+        }
+    }
+}
