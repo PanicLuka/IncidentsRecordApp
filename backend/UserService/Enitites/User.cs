c25922d82940ca07e5c5b63508827ff2fb3914f8
PanicLuka <lukap181@gmail.com>
2022-04-01 09:03:10 +0200

    User classes renamed

---
+++ backend/UserService/Enitites/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Enitites
+{
+    public class User
+    {
+        #region
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        
+        public int RoleId { get; set; }
+
+        public Role Role { get; set; }
+
+        #endregion
+    }
+}
