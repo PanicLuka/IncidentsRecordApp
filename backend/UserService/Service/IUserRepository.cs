d2c3e27d4067b7179e3d8ff0e422de1a2dd5e110
PanicLuka <lukap181@gmail.com>
2022-04-04 15:00:18 +0200

    interfaces for user and role refactored

---
+++ backend/UserService/Service/IUserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+
+namespace UserService.Data
+{
+    public interface IUserRepository
+    {
+        Task CreateUserAsync(User User);
+        
+        Task<List<User>> GetAllUsersAsync();
+
+        Task<User> GetUserByIdAsync(int UserId);
+
+        Task<User> GetUserByEmailAsync(string Email);
+        Task UpdateUserAsync(User User);
+
+        Task DeleteUserAsync(int UserId);
+
+        Task<int> GetRoleIdByUserEmail(string Email);
+
+        Task<bool> SaveChangesAsync();
+
+    }
+}
