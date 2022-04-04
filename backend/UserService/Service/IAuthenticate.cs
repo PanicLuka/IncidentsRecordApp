bed414ffc100fce7ea4d867cae8fa42f9cf41f92
PanicLuka <lukap181@gmail.com>
2022-04-04 15:01:24 +0200

    Authenticate interface created

---
+++ backend/UserService/Service/IAuthenticate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+using UserService.Models;
+
+namespace UserService.Service
+{
+    public interface IAuthenticate
+    {
+        Task<string> GenerateToken(UserLogin user); 
+    }
+}
