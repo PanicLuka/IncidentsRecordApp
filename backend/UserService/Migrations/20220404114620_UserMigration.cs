49506b88d0b1c56bdf8047ac29d73a31ffc0c84a
PanicLuka <lukap181@gmail.com>
2022-04-04 14:58:44 +0200

    Migrations

---
+++ backend/UserService/Migrations/20220404114620_UserMigration.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserService.Migrations
+{
+    public partial class UserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_roles", x => x.RoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "register",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoleId = table.Column<int>(type: "int", nullable: false, defaultValue: 2)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_register", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_register_roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "roles",
+                        principalColumn: "RoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "RoleId", "UserType" },
+                values: new object[] { 2, "User" });
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "RoleId", "UserType" },
+                values: new object[] { 1, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "register",
+                columns: new[] { "UserId", "Email", "FirstName", "LastName", "Password", "RoleId" },
+                values: new object[] { 1, "marko@gmail.com", "Marko", "Milic", "123456", 2 });
+
+            migrationBuilder.InsertData(
+                table: "register",
+                columns: new[] { "UserId", "Email", "FirstName", "LastName", "Password", "RoleId" },
+                values: new object[] { 2, "Nikola@gmail.com", "Nikola", "Milic", "123456", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_register_RoleId",
+                table: "register",
+                column: "RoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "register");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
