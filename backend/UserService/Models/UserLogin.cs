2f250b857738fc6e965ffcd3c4c784582989b013
PanicLuka <lukap181@gmail.com>
2022-04-04 14:59:06 +0200

    User class created

---
+++ backend/UserService/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Models
+{
+    public class UserLogin
+    {
+        #region
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        #endregion
+    }
+}
