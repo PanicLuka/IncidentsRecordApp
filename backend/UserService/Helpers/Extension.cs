3d57d69e33680ae52bb1df63b6134c86878e347a
PanicLuka <lukap181@gmail.com>
2022-04-04 14:58:24 +0200

    Helper methods for manual mapping implemented

---
+++ backend/UserService/Helpers/Extension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Enitites;
+using UserService.Models;
+
+namespace UserService.Helpers
+{
+    public static class Extension
+    {
+        public static UserDto UserToDto(this User user)
+        {
+            if (user != null)
+            {
+                return new UserDto
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Password = user.Password
+                };
+            }
+            return null;
+        }
+        public static User DtoToUser(this UserDto userDto)
+        {
+            if (userDto != null)
+            {
+                return new User
+                {
+                    FirstName = userDto.FirstName,
+                    LastName = userDto.LastName,
+                    Email = userDto.Email,
+                    Password = userDto.Password
+                };
+            }
+            return null;
+        }
+
+
+
+        public static RoleDto RoleToDto(this Role role)
+        {
+            if (role != null)
+            {
+                return new RoleDto
+                {
+                    UserType = role.UserType
+
+                };
+            }
+            return null;
+        }
+        public static Role DtoToRole(this RoleDto roleDto)
+        {
+            if (roleDto != null)
+            {
+                return new Role
+                {
+                    UserType = roleDto.UserType
+                };
+            }
+            return null;
+        }
+    }
+}
