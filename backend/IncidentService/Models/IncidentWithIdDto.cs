1d26a83ff875670b38eeeb4dc0cead22cf97e760
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-05 15:06:16 +0200

    Added new dto classes that have id

---
+++ backend/IncidentService/Models/IncidentWithIdDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IncidentService.Entities;
+
+namespace IncidentService.Models
+{
+    public class IncidentWithIdDto
+    {
+        public Guid IncidentId { get; set; }
+        public string Number { get; set; }
+        public int Significance { get; set; }
+        public string Workspace { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+        public bool ThirdPartyHelp { get; set; }
+        public string ProblemSolved { get; set; }
+        public bool FurtherAction { get; set; }
+        public string FurtherActionPerson { get; set; }
+        public string ActionDescription { get; set; }
+        public DateTime SolvingDate { get; set; }
+        public string Remarks { get; set; }
+        public string Verifies { get; set; }
+        public Guid UserId { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
