44ec690d4376827a0001e5481792cb16321fb36d
PanicLuka <lukap181@gmail.com>
2022-04-06 16:50:02 +0200

    GuidHelper class created

---
+++ backend/UserService/Helpers/GuidHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Helpers
+{
+    public class GuidHelper
+    {
+           
+        public static List<Guid> GetGuids()
+        {
+            List<Guid> guids = new List<Guid>();
+            Guid PermissionOneGuid = Guid.NewGuid();
+            guids.Add(PermissionOneGuid);
+            Guid PermissionTwoGuid = Guid.NewGuid();
+            guids.Add(PermissionTwoGuid);
+            Guid PermissionThreeGuid = Guid.NewGuid();
+            guids.Add(PermissionThreeGuid);
+            Guid PermissionFourGuid = Guid.NewGuid();
+            guids.Add(PermissionFourGuid);
+            Guid PermissionFiveGuid = Guid.NewGuid();
+            guids.Add(PermissionFiveGuid);
+            Guid PermissionSixGuid = Guid.NewGuid();
+            guids.Add(PermissionSixGuid);
+            Guid PermissionSevenGuid = Guid.NewGuid();
+            guids.Add(PermissionSevenGuid);
+            Guid PermissionEightGuid = Guid.NewGuid();
+            guids.Add(PermissionEightGuid);
+            Guid PermissionNineGuid = Guid.NewGuid();
+            guids.Add(PermissionNineGuid);
+            Guid PermissionTenGuid = Guid.NewGuid();
+            guids.Add(PermissionTenGuid);
+            Guid PermissionElevenGuid = Guid.NewGuid();
+            guids.Add(PermissionElevenGuid);
+
+            return guids;
+        }
+
+        
+
+
+
+
+    }
+}
