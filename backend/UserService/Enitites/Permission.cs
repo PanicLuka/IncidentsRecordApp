de5a94b390452c751941e9ee81691463a3bed441
PanicLuka <lukap181@gmail.com>
2022-04-06 16:50:40 +0200

    new classes created

---
+++ backend/UserService/Enitites/Permission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Enitites
+{
+    public class Permission
+    {
+        [Key]
+        public Guid PermissionId { get; set; }
+
+        public string AccessPermissions { get; set; }
+
+
+    }
+}
---
+++ backend/UserService/Enitites/RolePermission.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Enitites
+{
+    public class RolePermission
+    {
+        #region
+
+        public Guid? RoleId { get; set; }
+
+        public Role role { get; set; }
+
+        public Guid? PermissionId { get; set; }
+
+        public Permission permission { get; set; }
+
+
+        #endregion
+    }
+}
