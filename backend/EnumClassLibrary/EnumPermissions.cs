1015f6400404bda0fa98f916d498864d8c713aec
PanicLuka <lukap181@gmail.com>
2022-04-06 16:51:11 +0200

    custom class library created

---
+++ backend/EnumClassLibrary/EnumPermissions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnumClassLibrary
+{
+    
+    public class EnumPermissions
+    {
+        public enum Permissions 
+        {
+            CanAccessGetAllUsers = 0,
+            CanAccessDeleteUser = 1,
+            CanAccessUpdateUser = 2,
+            CanAccessGetByIdUser = 3,
+            CanAccessCreateUser = 4,
+            CanAccessGetAllIncidents = 5,
+            CanAccessGetIncidentById = 6,
+            CanAccessUpdateIncident = 7,
+            CanAccessDeleteIncident = 8,
+            CanAccessCreateIncident = 9,
+            CanPromoteToAdmin = 10
+        }
+    }
+}
