0d0968d8bacdd75b847b90edd809b8dd763f5208
PanicLuka <lukap181@gmail.com>
2022-04-07 13:28:27 +0200

    Manual class mapping for userPermission class created

--- backend/UserService/Helpers/Extension.cs
+++ backend/UserService/Helpers/Extension.cs
@@ -81,5 +81,31 @@ namespace UserService.Helpers
             }
             return null;
         }
+
+        public static UserPermissionDto UserPermissionToDto(this UserPermission userPermission)
+        {
+            if (userPermission != null)
+            {
+                return new UserPermissionDto
+                {
+                    UserId = userPermission.UserId,
+                    PermissionId = userPermission.PermissionId
+
+                };
+            }
+            return null;
+        }
+        public static UserPermission DtoToUserPermission(this UserPermissionDto userPermissionDto)
+        {
+            if (userPermissionDto != null)
+            {
+                return new UserPermission
+                {
+                    UserId = userPermissionDto.UserId,
+                    PermissionId = userPermissionDto.PermissionId
+                };
+            }
+            return null;
+        }
     }
 }
