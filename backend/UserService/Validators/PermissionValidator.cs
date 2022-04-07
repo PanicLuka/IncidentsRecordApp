981da12a0075ae8f7fa773478f3bc8cec53d076b
PanicLuka <lukap181@gmail.com>
2022-04-07 11:40:20 +0200

    Permission validator added

---
+++ backend/UserService/Validators/PermissionValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using UserService.Models;
+using EnumClassLibrary;
+using System.Collections.Generic;
+using System;
+
+namespace UserService.Validators
+{
+    public class PermissionValidator : AbstractValidator<PermissionDto>
+    {
+        public PermissionValidator()
+        {
+            List<string> conditions = new List<string>();
+
+            foreach (string i in Enum.GetNames(typeof(EnumPermissions.Permissions)))
+            {
+                conditions.Add(i);
+            }
+
+            RuleFor(x => x.AccessPermissions).Must(x => conditions.Contains(x))
+                .WithMessage("Significance can only be: " + String.Join(",", conditions) + "!");
+        }
+    }
+}
