9c8966f2f17765f55c5546faa9c4d1f4c3ec0cde
PanicLuka <lukap181@gmail.com>
2022-04-07 13:50:32 +0200

    Manual mapping class for RolePermission created

--- backend/UserService/Helpers/Extension.cs
+++ backend/UserService/Helpers/Extension.cs
@@ -107,5 +107,31 @@ namespace UserService.Helpers
             }
             return null;
         }
+
+        public static RolePermissionDto RolePermissionToDto(this RolePermission rolePermission)
+        {
+            if (rolePermission != null)
+            {
+                return new RolePermissionDto
+                {
+                    RoleId = rolePermission.RoleId,
+                    PermissionId = rolePermission.PermissionId
+
+                };
+            }
+            return null;
+        }
+        public static RolePermission DtoToRolePermission(this RolePermissionDto rolePermissionDto)
+        {
+            if (rolePermissionDto != null)
+            {
+                return new RolePermission
+                {
+                    RoleId = rolePermissionDto.RoleId,
+                    PermissionId = rolePermissionDto.PermissionId
+                };
+            }
+            return null;
+        }
     }
 }
