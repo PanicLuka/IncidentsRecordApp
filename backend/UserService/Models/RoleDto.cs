aff95433c39a3044a666e5da1e0301c48d7679ca
PanicLuka <lukap181@gmail.com>
2022-04-08 14:08:27 +0200

    removed unnecessary usings

--- backend/UserService/Models/RoleDto.cs
+++ backend/UserService/Models/RoleDto.cs
@@ -1,5 +1,4 @@
 ﻿
-
 namespace UserService.Models
 {
     public class RoleDto
--- backend/UserService/Service/IPermissionService.cs
+++ backend/UserService/Service/IPermissionService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using UserService.Enitites;
 using UserService.Models;
 
