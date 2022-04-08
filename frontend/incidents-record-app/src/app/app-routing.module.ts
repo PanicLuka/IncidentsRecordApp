436cd7db5c0537e2db99035a2ddc5559d1746a90
Kristijan Stošić <nimeria83@yahoo.com>
2022-04-08 10:57:04 +0200

    16-frontend-installing components and setting up angular material

--- frontend/incidents-record-app/src/app/app-routing.module.ts
+++ frontend/incidents-record-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IncidentComponent } from './components/incident/incident.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReportComponent } from './components/report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'incident', component: IncidentComponent },
+  { path: 'report', component: ReportComponent },
+  { path: '', pathMatch: 'full', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/incidents-record-app/src/app/app.module.ts
+++ frontend/incidents-record-app/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
-
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Components
+import { LoginComponent } from './components/login/login.component';
+import { IncidentComponent } from './components/incident/incident.component';
+import { ReportComponent } from './components/report/report.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    IncidentComponent,
+    ReportComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
