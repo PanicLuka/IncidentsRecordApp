643b8f36c9aca1d2d8995d596c50db592a15347d
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-11 11:32:38 +0200

    Split class for mapping

--- backend/IncidentService/Helpers/IncidentExtension.cs
+++ backend/IncidentService/Helpers/IncidentExtension.cs
@@ -1,85 +1,10 @@
 ﻿using IncidentService.Entities;
 using IncidentService.Models;
 
-namespace IncidentService
-    .Helpers
+namespace IncidentService.Helpers
 {
-    public static class Extension
+    public static class IncidentExtension
     {
-        public static CategoryDto CategoryToDto(this Category category)
-        {
-            if (category != null)
-            {
-                return new CategoryDto
-                {
-                    CategoryName = category.CategoryName
-                };
-            }
-            return null;
-        }
-
-        public static Category DtoToCategory(this CategoryDto categoryDto)
-        {
-            if (categoryDto != null)
-            {
-                return new Category
-                {
-                    CategoryName = categoryDto.CategoryName
-                };
-            }
-            return null;
-        }
-
-        public static CategoryDto CategoryWithIdDtoToDto(this CategoryWithIdDto categoryWithIdDto)
-        {
-            if (categoryWithIdDto != null)
-            {
-                return new CategoryDto
-                {
-                    CategoryName = categoryWithIdDto.CategoryName
-                };
-            }
-            return null;
-        }
-
-        public static CategoryWithIdDto DtoToCategoryWithIdDto(this CategoryDto categoryDto)
-        {
-            if (categoryDto != null)
-            {
-                return new CategoryWithIdDto
-                {
-                    CategoryName = categoryDto.CategoryName
-                };
-            }
-            return null;
-        }
-
-        public static Category CategoryWithIdDtoToCategory(this CategoryWithIdDto categoryWithIdDto)
-        {
-            if (categoryWithIdDto != null)
-            {
-                return new Category
-                {
-                    CategoryId = categoryWithIdDto.CategoryId,
-                    CategoryName = categoryWithIdDto.CategoryName
-                };
-            }
-            return null;
-        }
-
-        public static CategoryWithIdDto CategoryToCategoryWithIdDto(this Category category)
-        {
-            if (category != null)
-            {
-                return new CategoryWithIdDto
-                {
-                    CategoryId = category.CategoryId,
-                    CategoryName = category.CategoryName
-                };
-            }
-            return null;
-        }
-
         public static IncidentDto IncidentToDto(this Incident incident)
         {
             if (incident != null)
