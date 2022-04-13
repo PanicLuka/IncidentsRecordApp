ec0153329eda580284106b886e166d5611919a31
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-13 12:25:53 +0200

    Added authentication attribute

---
+++ backend/IncidentService/Attributes/MicroserviceAuthAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IncidentService.Attributes
+{
+    public class MicroserviceAuthAttribute : ActionFilterAttribute
+    {
+        private const string ApiKeyValue = "";
+
+        public MicroserviceAuthAttribute() : base()
+        {
+
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var ApiKeyValue = configuration.GetValue<string>("APIkey:Key");
+            var headers = context.HttpContext.Request.Headers;
+
+            if (headers["apiKey"] == ApiKeyValue)
+            {
+                return;
+            }
+
+            context.Result = new ContentResult()
+            {
+                StatusCode = 401,
+                Content = "Api key not provided"
+
+            };
+        }
+    }
+}
