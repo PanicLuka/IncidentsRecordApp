6b49bcff2fd86cdeb0743e6e5e40d77da1b09052
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-15 10:40:14 +0200

    Added abstract base controller

---
+++ backend/IncidentService/Helpers/Config.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IncidentService.Helpers
+{
+    public class Config
+    {
+        private readonly IConfiguration _configuration;
+
+        public Config(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string GetAuthorization()
+        {
+            var strAuthorization = _configuration["Authorization:Authorization"].ToString();
+            return strAuthorization;
+        }
+    }
+}
