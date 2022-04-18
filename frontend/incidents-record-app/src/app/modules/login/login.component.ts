d93b0bd38b5b465a73dc81c214efe89343a7f69d
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-18 09:42:06 +0200

    implemented method for login

--- frontend/incidents-record-app/src/app/modules/login/login.component.ts
+++ frontend/incidents-record-app/src/app/modules/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 import { UserService } from '../shared/services/user.service';
 
 @Component({
@@ -8,8 +12,55 @@ import { UserService } from '../shared/services/user.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  invalidLogin!:boolean;
+  email!: string;
+  password!: string;
+  isLoading = false;
+  invalidLoginMess!: boolean;
+  user!: User;
+
+  constructor(public userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  public login(form: NgForm) {
+    this.email = form.value.email;
+    this.password = form.value.password;
+
+    const credentials = {
+      "email": this.email,
+      "password": this.password
+    }
+
+    this.user.email=credentials.email;
+    this.user.password=credentials.password;
+
+    this.userService.loginUser(this.user)
+      .subscribe(response => {
+
+        const token = (<any>response).token;
+
+
+        localStorage.setItem("JWT ", token);
+
+        this.invalidLogin = false;
+        this.invalidLoginMess = false;
+
+        form.reset();
+
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        },
+          2500);
+
+      }), (error: Error) => {
+        this.invalidLogin = true;
+      };
+  }
+
+  removeMessage() {
+    this.invalidLoginMess = true;
+  }
+
 }
