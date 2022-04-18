71e2d5ccd462be7d9abe2a46133c3b3866fe5d20
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-04-18 14:51:02 +0200

    Implemented logout

--- frontend/incidents-record-app/src/app/modules/shared/components/navbar/navbar.component.ts
+++ frontend/incidents-record-app/src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated!: boolean;
+  constructor(public userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  isLoggedIn() {
+    this.isAuthenticated = this.userService.isAuthenticated();
+  }
+
+  public logout() {
+    localStorage.removeItem("JWT_NAME");
+    this.router.navigate(['/login']);
+  }
 }
