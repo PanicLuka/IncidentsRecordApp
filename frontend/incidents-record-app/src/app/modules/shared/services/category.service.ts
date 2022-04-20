8dcb438d0d5121f69836502d59206bc2927860ee
PanicLuka <lukap181@gmail.com>
2022-04-20 16:34:01 +0200

    category service added

---
+++ frontend/incidents-record-app/src/app/modules/shared/services/category.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/models/category.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) {
+
+  }
+  public getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.gateway}/gateway/get/categories`);
+  }
+
+
+}
