72e53562dda051465fb8f89e4ee54c681ff8e86e
nikola-aleksic <56644651+nikola-aleksic@users.noreply.github.com>
2022-05-02 13:48:00 +0200

    Minor change

--- frontend/incidents-record-app/src/app/app-routing.module.ts
+++ frontend/incidents-record-app/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'incident', loadChildren: () => import('./modules/incident/incident.module').then((m) => m.IncidentModule), canActivate: [AuthGuard] },
   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
-  { path: 'category', loadChildren: () => import('./modules/category/category.module').then(m => m.CategoryModule) },
+  { path: 'category', loadChildren: () => import('./modules/category/category.module').then(m => m.CategoryModule), canActivate: [AuthGuard] },
 ];
 
 @NgModule({
